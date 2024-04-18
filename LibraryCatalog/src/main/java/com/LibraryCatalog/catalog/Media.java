c576faae432f354c9bdb36d7a59c35b4051b76c4
Ben Heney <ben@dark-star.lan>
2024-04-17 21:06:30 -0400

    Stub out Patron Kiosk form

---
+++ LibraryCatalog/src/main/java/com/LibraryCatalog/catalog/Media.java
@@ -0,0 +1,4 @@
+package main.java.com.LibraryCatalog.catalog;
+
+public interface Media {
+}
---
+++ LibraryCatalog/src/main/java/com/LibraryCatalog/ui/patron/KioskHome.java
@@ -0,0 +1,16 @@
+package main.java.com.LibraryCatalog.ui.patron;
+
+import javax.swing.*;
+
+public class KioskHome {
+    private JLabel lblSignIn;
+    private JLabel lblCardNumber;
+    private JTextField textCardNubmer;
+    private JLabel lblPassword;
+    private JPasswordField pswdPassword;
+    private JButton btnSignIn;
+    private JLabel lblSearch;
+    private JComboBox comboMediaType;
+    private JButton btnSearch;
+    private JButton btnAddCriterion;
+}
