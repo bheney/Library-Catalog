ebb90311a4c37abdca41419fdc310392d6fba7f8
Charishma95 <Charishma.Bandari@unh.edu>
2024-04-27 12:49:46 -0400

    Add user authentication methods to User.java

---
+++ LibraryCatalog/src/main/java/com/LibraryCatalog/User.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+ublic class User {
+    // Attributes
+    private String firstName;
+    private String lastName;
+    private Date dob;
+    private String street;
+    private String phone;
+    private String libraryCardNumber;
+    private String issue;
+    private String email;
+    private String zip;
+
+    // Constructor
+    public User(String firstName, String lastName, Date dob, String street, String phone, String libraryCardNumber, String issue, String email, String zip) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.street = street;
+        this.phone = phone;
+        this.libraryCardNumber = libraryCardNumber;
+        this.issue = issue;
+        this.email = email;
+        this.zip = zip;
+    }
